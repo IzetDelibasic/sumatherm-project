0293f48b4796d720cc0b608399b6cd717b08639c
Izet Delibasic <izetdelibasic55@gmail.com>
2023-12-24 02:04:47 +0100

    Fixing problem with subscribe button

--- app/components/SubscribeButton/SubscribeButton.tsx
+++ app/components/SubscribeButton/SubscribeButton.tsx
@@ -10,7 +10,7 @@ const SubscribeButton = () => {
   return (
         <div className='flex items-center justify-center w-full md:w-[25rem] md:mx-auto lg:mx-0'>
           <Button
-              className="relative bg-red-600 text-white border-[1px] border-black font-medium lg:py-[1rem] py-[1rem] lg:px-[2rem] px-[5rem] md:px-[12rem] rounded-[3rem] group overflow-hidden z-[1] hover:border-opacity-0 hover:border-transparent mb-[1rem] lg:mb-0"
+              className="relative bg-black hover:bg-red-600 text-white border-[1px] border-black font-medium lg:py-[1rem] py-[1rem] lg:px-[2rem] px-[5rem] md:px-[12rem] rounded-[3rem] group overflow-hidden z-[1] hover:border-opacity-0 hover:border-transparent mb-[1rem] lg:mb-0"
               iconClassName=""
               title='Subscribe'
               titleClassName="group-hover:text-white"
